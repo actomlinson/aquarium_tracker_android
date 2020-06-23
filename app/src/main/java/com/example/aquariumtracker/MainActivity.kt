cfc81f740b7927154b0f14694929c3fb0906381d
Amanda Tomlinson <axtomlinson@gmail.com>
2020-06-22 22:49:56 -0700

    layout and navigation mostly complete

--- app/src/main/java/com/example/aquariumtracker/MainActivity.kt
+++ app/src/main/java/com/example/aquariumtracker/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.aquariumtracker
 
 import android.os.Bundle
+import android.view.Gravity
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
@@ -17,23 +19,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
         val navController = findNavController(R.id.nav_host_fragment)
-        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        val appBarConfiguration = AppBarConfiguration(navController.graph, findViewById(R.id.drawer_layout))
 
         findViewById<NavigationView>(R.id.nav_view).setupWithNavController(navController)
         findViewById<Toolbar>(R.id.toolbar).setupWithNavController(navController, appBarConfiguration)
 
-
-//       setSupportActionBar(findViewById(R.id.toolbar))
-
-//        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                    .setAction("Action", null).show()
         }
 
 //        val queue = Volley.newRequestQueue(this)
-//        val url = "http://192.168.1.17:8080/api/users/"
+//        val url = "***"
 //        val textView = findViewById<TextView>(R.id.textview_main)
 //        val stringRequest = StringRequest(
 //                Request.Method.GET,
@@ -52,7 +47,15 @@ class MainActivity : AppCompatActivity() {
 //        queue.add(stringRequest)
 //        Log.i("main", "added to queue")
 
+    override fun onBackPressed() {
+        val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
 
+        if (drawer.isDrawerOpen(Gravity.LEFT)) {
+            drawer.closeDrawer(Gravity.LEFT)
+        } else {
+            drawer.openDrawer(Gravity.LEFT)
+        }
+    }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
